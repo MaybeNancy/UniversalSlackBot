1d14681d8108b06d773f9e1a8d76e377e2566431
Nancy Orren <nancy.navel@protonmail.com>
2026-01-29 05:36:32 +0000

    Update brian.py
    new prototype code. :>

--- brian.py
+++ brian.py
@@ -1,3 +1,24 @@
-import os
+#Dependencies
 
-//hello :>!
+import os
+from flask import Flask, request
+from slack_bolt import App
+from slack_bolt.adapter.flask import SlackRequestHandler
+
+#app init
+
+bolt_app = App(xoxb=os.getenv("XOXB"),secret=os.getenv("SECRET"))
+
+#functionality
+@bolt_app.message("hi")
+def reply(message,say):
+  user = message['user']
+  say(f"Hey <@{user}>! 👋")
+
+#flask wrapper
+flask_app = Flask(__name__)
+handler = SlackRequestHandler(bolt_app)
+
+@flask_app.route("/slack/events", methods=["POST"])
+def slack_events():
+    return handler.handle(request)
