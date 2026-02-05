5fd671da714a27d10f8c98bcf0f0ea76dc778713
Nancy Orren <nancy.navel@protonmail.com>
2026-02-05 17:09:34 +0000

    Update app.py

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ def slack_events():
     data = request.json
     channel_id = data['event']['channel']
     user_id = data['event']['user']
-    ts = data['event']['ts']
+    #ts = data['event']['ts']
 
     if user_id == ADMIN:
         SendMessage(channel_id, data)
