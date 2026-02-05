6d87207341ccee9e46da239c4e77a69db1bd944f
Nancy Orren <nancy.navel@protonmail.com>
2026-02-05 18:41:40 +0000

    Update app.py
    string

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 ADMIN = "U0AAS5ZGSAD"
 BOT = "U0ABJJQ288M"
 
-temp_bot_msg_timer = 0
+temp_bot_msg_timer = ""
 
 # Environment variables for your Slack Token and Signing Secret
 SLACK_BOT_TOKEN = os.getenv('SLACK_BOT_TOKEN')
@@ -115,6 +115,6 @@ def slack_commands():
     response_text = data
 
     b_msg = SendMessage(channel,response_text)
-    temp_bot_msg_timer = b_msg.get('event', {}).get('ts')
+    temp_bot_msg_timer = b_msg["ts"]
 
     return jsonify({"response_type": "in_channel", "text": "🧠👍"})
