3c1a0739d1433aa88396ca774bcce598875277d2
Nancy Orren <nancy.navel@protonmail.com>
2026-03-10 21:15:10 +0000

    Update ping.py
    pong

--- src/handlers/ping.py
+++ src/handlers/ping.py
@@ -1,1 +1,8 @@
 #pong
+# src/handlers/ping.py
+def register(dispatcher):
+    dispatcher.register("app_mention", handle_ping)
+
+async def handle_ping(payload, slack, store):
+    channel = payload["event"]["channel"]
+    await slack.post_message(channel, "pong")
