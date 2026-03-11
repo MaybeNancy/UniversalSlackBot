9a942b5df89bf1a67a6ec825124e2ae2e4c28593
Nancy Orren <nancy.navel@protonmail.com>
2026-03-11 03:54:02 +0000

    Update logging.py
    8 Docu Later

--- src/utils/logging.py
+++ src/utils/logging.py
@@ -1,6 +1,6 @@
-# src/utils/logging.py
 import structlog
 
 def get_logger(name: str = "duckbot"):
+    # Returns a structlog logger instance; attach to app/state where needed
     logger = structlog.get_logger(name)
     return logger
