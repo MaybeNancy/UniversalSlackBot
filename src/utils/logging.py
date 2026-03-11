f0f8ea80f3b85a80398b0bd1c1621d1f07445fbc
Nancy Orren <nancy.navel@protonmail.com>
2026-03-11 05:27:02 +0000

    Update logging.py
    some noted to remember deletion

--- src/utils/logging.py
+++ src/utils/logging.py
@@ -4,3 +4,5 @@ def get_logger(name: str = "duckbot"):
     # Returns a structlog logger instance; attach to app/state where needed
     logger = structlog.get_logger(name)
     return logger
+#TO BE REMOVED, RAILWAY ALREADY
+#TELLS WHATS WRONG
