b586dfe7dfe4bd4d8056b29335f673f0b2e314ab
Nancy Orren <nancy.navel@protonmail.com>
2026-03-11 19:09:01 +0000

    Update logging.py
    info

--- src/utils/logging.py
+++ src/utils/logging.py
@@ -1,8 +1,11 @@
+"""
+I will change this entirely,
+I need to catch bugs with slack itself.
+Also needs to work with JSON.
+"""
 import structlog
 
 def get_logger(name: str = "duckbot"):
     # Returns a structlog logger instance; attach to app/state where needed
     logger = structlog.get_logger(name)
     return logger
-#TO BE REMOVED, RAILWAY ALREADY
-#TELLS WHATS WRONG
