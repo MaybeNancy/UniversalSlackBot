b19c05be7845cc143fd9f2f37bdbd88b5acc9553
Nancy Orren <nancy.navel@protonmail.com>
2026-03-11 00:37:58 +0000

    Update ping.py
    pong

--- src/handlers/ping.py
+++ src/handlers/ping.py
@@ -1,8 +1,8 @@
-#pong
 # src/handlers/ping.py
 def register(dispatcher):
     dispatcher.register("app_mention", handle_ping)
 
-async def handle_ping(payload, slack, store):
+async def handle_ping(ctx, payload):
     channel = payload["event"]["channel"]
-    await slack.post_message(channel, "pong")
+    await ctx.slack.post_message(channel, "pong")
+    ctx.logger.info("Ping responded", channel=channel)
