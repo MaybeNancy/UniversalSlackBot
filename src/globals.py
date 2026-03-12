ddaea713a43df5b295190d0865de44b3a72e7fa2
Nancy Orren <nancy.navel@protonmail.com>
2026-03-12 01:04:57 +0000

    Update globals.py
    acincio

--- src/globals.py
+++ src/globals.py
@@ -1,8 +1,12 @@
+import asyncio
 #httpx or session
 client = None
 #semaphore
 
 def start_globals():
-  client = httpx.AssyncC()
+  TIMEOUT = httpx.Timeout(8.0, connect=2.0)
+  LIMITS = httpx.Limits(max_connections=10,max_keepalive_connections=5)
+  client = httpx.AsyncClient(timeout=TIMEOUT, limit=LIMITS)
 
 def end_globals()
+  client.aclose()
