5f6979797bb42530246609e49ebeaca02221d564
Nancy Orren <nancy.navel@protonmail.com>
2026-03-12 01:03:59 +0000

    Update server.py
    :>

--- src/server.py
+++ src/server.py
@@ -3,6 +3,8 @@ import os
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 
+import globals
+
 """
 Fix or use these later:
 from src.routes import router
@@ -14,17 +16,16 @@ client = None
 
 def create_app():
     #We init the client
-    TIMEOUT = httpx.Timeout(8.0, connect=2.0)
-    LIMITS = httpx.Limits(max_connections=10,max_keepalive_connections=5)
-    client = httpx.AsyncClient(timeout=TIMEOUT, limit=LIMITS)
 
     @asynccontextmanager
     async def lifespan(app: FastAPI):
         #InitDispatcher
         #Semaphoren thingy
         #Slack init?
+        globals_start()
         yield
-        client.aclose()
+        globals_end()
+        #client.aclose()
         
     app = FastAPI(title="Universal Slack Bot",lifespan=lifespan)
 
