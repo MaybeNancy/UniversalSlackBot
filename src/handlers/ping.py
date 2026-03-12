e411907ea80eaae2c57ac9e784f43b13af7aa70f
Nancy Orren <nancy.navel@protonmail.com>
2026-03-12 23:18:09 +0000

    Update ping.py
    asyncyio

--- src/handlers/ping.py
+++ src/handlers/ping.py
@@ -3,12 +3,14 @@ I will modify this later,
 I just need the thing working
 """
 
+import asyncio
+
 from ..services.slack_service import send_message
 
-def reply(data):
+async def reply(data):
     # Example handler: respond "pong" when bot is mentioned
     channel = data["channel"]
-    send_message(channel, "Hey")
+    await send_message(channel, "Hey")
 
     return {"status":"ok"}
     # Use ctx.logger if present; fallback to ctx.slack.logger
