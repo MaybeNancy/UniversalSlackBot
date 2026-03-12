4ed64a69e9b9675434c1d3d879d94441da9157fc
Nancy Orren <nancy.navel@protonmail.com>
2026-03-12 17:08:16 +0000

    Update globals.py
    cred

--- src/globals.py
+++ src/globals.py
@@ -5,14 +5,19 @@ client = None
 #semaphore
 
 def globals_start():
+  #aquire the slack credentiaks
+  #da credentials
+  #twitter credentials
+  #perchance credentials?
   TIMEOUT = httpx.Timeout(8.0, connect=2.0)
   LIMITS = httpx.Limits(max_connections=10,max_keepalive_connections=5)
   
   global client
   client = httpx.AsyncClient(timeout=TIMEOUT, limits=LIMITS)
+  #start semaphore
 
 def globals_end():
-  global client
   client.aclose()
+  #end semaphore
 
 def return_client(): return client
