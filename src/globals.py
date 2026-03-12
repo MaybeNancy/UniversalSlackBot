e043cab30708fcdd9260e964760c22a9cad6973a
Nancy Orren <nancy.navel@protonmail.com>
2026-03-12 17:05:17 +0000

    Update globals.py
    global client

--- src/globals.py
+++ src/globals.py
@@ -7,9 +7,12 @@ client = None
 def globals_start():
   TIMEOUT = httpx.Timeout(8.0, connect=2.0)
   LIMITS = httpx.Limits(max_connections=10,max_keepalive_connections=5)
+  
+  global client
   client = httpx.AsyncClient(timeout=TIMEOUT, limits=LIMITS)
 
 def globals_end():
+  global client
   client.aclose()
 
 def return_client(): return client
