65862f01c0b34ec25ff3ad661e1d1b50f058fa6d
Nancy Orren <nancy.navel@protonmail.com>
2026-03-13 01:23:38 +0000

    Update ping.py
    hope

--- src/handlers/ping.py
+++ src/handlers/ping.py
@@ -4,10 +4,11 @@ I just need the thing working
 """
 import asyncio
 
-from ..services.slack_service import send_message
+from ..services.slack_service import send_message, new_name
 
 async def reply(data):
     # Example handler: respond "pong" when bot is mentioned
+    await new_name()
     channel = data["channel"]
     text = "Hello! new me, new everything, work in progress, comeback later :nancy-wink:"
     return await send_message(channel, text)
