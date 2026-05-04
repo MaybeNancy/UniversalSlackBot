5ed6c919be33125e14b644f6f6ed02723e437e76
Nancy Orren <nancy.navel@protonmail.com>
2026-05-04 01:55:26 -0600

    Update perchance.py
    test

--- src/services/perchance.py
+++ src/services/perchance.py
@@ -1,6 +1,16 @@
+import asyncio
 from ..globals import return_client
 
 link = "https://perchance.org/nancy-ai-module"
 
-def call_site():
-  return "hello"
+async def call_site():
+  client = return_client()
+  
+  data="Hi"
+  
+  resp = await client.post(
+            link,
+            json=data,
+  )
+    
+  return resp
