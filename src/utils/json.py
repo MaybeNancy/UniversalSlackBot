52d836a536a731eb693e7fcdeafecfbcd842d255
Nancy Orren <nancy.navel@protonmail.com>
2026-05-03 22:13:51 -0600

    Update json.py
    Updated and finalized initial validator

--- src/utils/json.py
+++ src/utils/json.py
@@ -5,7 +5,25 @@ any json
 from jsonschema import validate
 
 schema = {
-  
+  "type": "object"
+  "properties":{
+     "type": {
+       "type" "string"
+     },
+     "token": {
+       "type": "string"
+     },
+     "team_id": {
+       "type": "string"
+     },
+     "api_app_id":{
+       "type": "string"
+     },
+     "event": {
+       "type": "object"
+     }
+  },
+  "required": ["type","token","team_id","api_app_id","event"]
 }
 
 def checkjson(json):
