8097d175614f5372d2b3542989bb29a890a1bf0a
Nancy Orren <nancy.navel@protonmail.com>
2026-05-04 02:43:05 -0600

    Update perchance.py
    check

--- src/services/perchance.py
+++ src/services/perchance.py
@@ -7,10 +7,16 @@ async def call_site():
   client = return_client()
   
   data="Hi"
-  
+
+  """
   resp = await client.post(
             link,
             json=data,
   )
-    
+  """
+  resp = await client.get(link)
+  print(resp.status_code)
+  print(await resp.text())
+
+  
   return resp
