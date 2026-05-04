7defcc787659e7c9184716655d4d88e8c975e5d3
Nancy Orren <nancy.navel@protonmail.com>
2026-05-03 22:46:57 -0600

    Update json.py
    fixed stuff

--- src/utils/json.py
+++ src/utils/json.py
@@ -7,27 +7,17 @@ from jsonschema import validate
 schema = {
   "type": "object"
   "properties":{
-     "type": {
-       "type" "string"
-     },
-     "token": {
-       "type": "string"
-     },
-     "team_id": {
-       "type": "string"
-     },
-     "api_app_id":{
-       "type": "string"
-     },
-     "event": {
-       "type": "object"
-     }
+     "type": {"type": "string"},
+     "token": {"type": "string"},
+     "team_id": {"type": "string"},
+     "api_app_id":{"type": "string"},
+     "event": {"type": "object"}
   },
   "required": ["type","token","team_id","api_app_id","event"]
 }
 
 def checkjson(json):
   try: 
-    validate(json, schema)
+    validate(instance=json,schema=schema)
   except: 
       raise HTTPException(status_code=401, detail="incorrect json")
