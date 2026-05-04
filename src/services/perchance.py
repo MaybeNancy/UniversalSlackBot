8c20ff61c93b35d690aa8b0b3e59b79145d6d20a
Nancy Orren <nancy.navel@protonmail.com>
2026-05-04 02:53:54 -0600

    Update perchance.py
    other checks

--- src/services/perchance.py
+++ src/services/perchance.py
@@ -5,13 +5,19 @@ link = "https://perchance.org/nancy-ai-module"
 
 async def call_site():
   client = return_client()
+
+  headers={
+    "User-Agent": "MyBot/1.0",
+    "Referer": "https://perchance.org/"
+  }
   
-  data="Hi"
+  data={"data":"hi"}
 
   """
   resp = await client.post(
             link,
             json=data,
+            headers=headers
   )
   """
   resp = await client.get(link)
