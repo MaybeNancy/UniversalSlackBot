4307b70486271f42dae279ad7f540904cc93f0d1
Nancy Orren <nancy.navel@protonmail.com>
2026-05-04 00:25:16 -0600

    Update ping.py
    #

--- src/handlers/ping.py
+++ src/handlers/ping.py
@@ -8,7 +8,7 @@ from ..services.slack_service import send_message, new_name
 
 async def reply(data):
     # Example handler: respond "pong" when bot is mentioned
-    await new_name()
+    #await new_name()
     channel = data["channel"]
     text = "Hello! new me, new everything, work in progress, comeback later :nancy-wink:"
     return await send_message(channel, text)
