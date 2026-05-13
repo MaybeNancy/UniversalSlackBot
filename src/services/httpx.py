74034ebfe7de5ba8231d13363bfa06a2f239f832
Nancy Orren <nancy.navel@protonmail.com>
2026-05-13 00:58:52 +0000

    Update httpx.py
    fixed

--- src/services/httpx.py
+++ src/services/httpx.py
@@ -1,27 +1,14 @@
 """
 Singleton for httpx stuff
 """
-import httpx, json, asyncio
+import httpx, asyncio
 
 from ..globals import return_client, return_b_token
 
-
-    url = BASE_URL+url_add
-
-    if n_headers == None:
-        headers={
-            "Authorization": f"Bearer {return_b_token()}",
-            "Content-Type": "application/json; charset=utf-8"
-        }
-    else:
-        headers=n_headers
-    data = n_data
-
+async def post(url,headers,data):
     client = return_client()
-    resp = await client.post(
+    return await client.post(
             url,
             json=data,
             headers=headers
     )
-    
-    return resp.json()
