4f74f97d1079c1f2b7b00808bf546100b5895a8d
Nancy Orren <nancy.navel@protonmail.com>
2026-05-31 11:59:10 -0600

    Create nancyfy.py
    new stuff

---
+++ src/utils/nancyfy.py
@@ -0,0 +1,11 @@
+"""
+Tool made specifically
+to add nancy emojis
+to any text given
+based on the tone of the
+message, or just to replace
+symbols in a funny way :>
+"""
+
+def nancyfy(text):
+  return text+"nancy-wink"
