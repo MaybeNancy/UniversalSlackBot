12ab79818543954f2b52828ab1a5a6a5f632cb60
Nancy Orren <nancy.navel@protonmail.com>
2026-06-01 22:46:02 -0600

    Create msg.py
    file created

---
+++ src/handlers/msg.py
@@ -0,0 +1,10 @@
+import asyncio
+
+from ..services.slack import edit_message
+
+from ..utils.nancyfy import nancyfy
+
+async def check_msg(data):
+    channel = data["channel"]
+    text = nancyfy(data["text"])
+    return await send_message(channel, text)
