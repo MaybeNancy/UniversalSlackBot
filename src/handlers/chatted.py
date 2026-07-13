a1845a5c3055c9cc45bfb5a118304f5137f7d856
Nancy Orren <nancy.navel@protonmail.com>
2026-07-13 01:12:35 -0600

    Update chatted.py
    >)

--- src/handlers/chatted.py
+++ src/handlers/chatted.py
@@ -17,14 +17,14 @@ async def talk(data):
     text = data["text"]
     prompt="Say hello to "+str(user)
     print(user)
-    text = str(call_ai(prompt)))
+    text = str(call_ai(prompt))
     return await send_message(channel, text)
 
 async def get_message(data):
     r = random.randint(1,3)
     if(r == 1):
         return await emojify(data)
-    if(r>1):
+    if(r>=1):
         return await talk(data)
     
     return {"status":"ok"}
