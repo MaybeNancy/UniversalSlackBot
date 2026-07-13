dd97fd860d22563ebe5724565bef2dc653af528c
Nancy Orren <nancy.navel@protonmail.com>
2026-07-13 00:34:44 -0600

    Update chatted.py
    chat part1

--- src/handlers/chatted.py
+++ src/handlers/chatted.py
@@ -1,13 +1,24 @@
 import asyncio, random
 
 from ..services.slack import react
+from ..services.ai import call_ai
 
 from ..utils.nancyfy import nancymoji
 
 async def emojify(data):
-    if(random.randint(1,3) == 1):
-        channel = data["channel"]
-        ts = data["ts"]
-        return await react(channel, nancymoji(),ts)
+    channel = data["channel"]
+    ts = data["ts"]
+    return await react(channel, nancymoji(),ts)
+
+async def talk(data):
+    return {"status":"ok"}
+
+async def get_message(data):
+    r = random.randint(1,3)
+    if(r == 1):
+        return emojify(data)
+    elif(r>1):
+        return talk(data)
     else:
-        return {"status": "ok"}
+        return {"status":"ok"}
+        
