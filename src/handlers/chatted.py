1c5c3052a5da4567aa323c91eb9c517302267b65
Nancy Orren <nancy.navel@protonmail.com>
2026-07-13 00:35:49 -0600

    Update chatted.py
    1.5

--- src/handlers/chatted.py
+++ src/handlers/chatted.py
@@ -17,8 +17,7 @@ async def get_message(data):
     r = random.randint(1,3)
     if(r == 1):
         return emojify(data)
-    elif(r>1):
+    if(r>1):
         return talk(data)
-    else:
-        return {"status":"ok"}
-        
+    
+    return {"status":"ok"}
