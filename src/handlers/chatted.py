08178afaef69ab17180cbf5658b84c8d9df4d88f
Nancy Orren <nancy.navel@protonmail.com>
2026-07-13 01:49:41 -0600

    Update chatted.py
    <-

--- src/handlers/chatted.py
+++ src/handlers/chatted.py
@@ -11,11 +11,11 @@ async def emojify(data):
     return await react(channel, nancymoji(),ts)
 
 async def talk(data):
-    
     channel = data["channel"]
     user = data["user"]
     text = data["text"]
     prompt="Say hello to Nancy"
+    print(user)
     print(await get_user(user))
     text = str(call_ai(prompt))
     return await send_message(channel, text)
